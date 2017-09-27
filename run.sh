a1804a2b717a612c505713027c7cd7efb1a540e0
Jonathan Gould <jonathan@k4connect.com>
2017-09-27 09:49:57 -0400

    Updated token

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-mvn exec:java -Dexec.mainClass="org.thingsboard.gateway.GatewayApplication"
+sed -i '' 's/true/false/' target/classes/tb-gateway.yml
+mvn exec:java -Dexec.mainClass="org.thingsboard.gateway.GatewayApplication" -DGATEWAY_HOST="localhost" -DGATEWAY_ACCESS_TOKEN="ixIqRBX2KoAEId6yDpIl"
