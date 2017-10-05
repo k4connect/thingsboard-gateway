0a9f54ce0c1a50ce57e1bdf83b3eb2fe22eebcfb
Jonathan Gould <jonathan@k4connect.com>
2017-10-05 13:38:50 -0400

    Exit on build fail

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+set -e
+
 mvn compile
 mvn exec:java -Dexec.mainClass="org.thingsboard.gateway.GatewayApplication" \
 	-DGATEWAY_HOST="localhost" \
-	-DGATEWAY_ACCESS_TOKEN="ixIqRBX2KoAEId6yDpIl" \
+	-DGATEWAY_ACCESS_TOKEN="tB1nPdvJBeCwWUmS10qk" \
 	-Dlogging.level.org.springframework.boot.autoconfigure.logging=ERROR
